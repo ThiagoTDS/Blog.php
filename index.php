a98c621efa290a4f11cb123181f29659b0a8ce12
Thiago Dias <tdsdias.tc3@gmail.com>
2023-11-22 22:40:31 -0300

    create footer-header

--- index.php
+++ index.php
@@ -1,26 +1,7 @@
 <?php
-    include_once("helpers/url.php")
+    include_once("templates/header.php");
 ?>
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Blog Codar</title>
-    <!--ESTILOS DO PROJETO-->
-    <link rel="stylesheet" href="<?= $BASE_URL?>/CSS/styles.css">
-    <!--GOOGLE FONTS-->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;700&family=Poppins:ital,wght@0,200;0,300;0,400;0,500;1,100&display=swap"
-        rel="stylesheet">
-
-</head>
-
-<body>
-    <h1>Ola Mundo</h1>
-</body>
-
-</html>
+<h1>Olá Mundo</h1>
+<?php
+include_once("templates/footer.php")
+?>
